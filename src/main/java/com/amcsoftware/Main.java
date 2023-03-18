a2ac54ffe803c2aa700b742802efc7e2e8f571fa
vanessadixon1 <mclendonfamily88@gmail.com>
2023-03-18 11:07:56 -0400

    created menu

---
+++ src/main/java/com/amcsoftware/Main.java
@@ -0,0 +1,15 @@
+package com.amcsoftware;
+
+public class Main {
+    public static void main(String[] args) {
+
+         menu();
+    }
+
+    public static void menu() {
+        System.out.println(
+              "1️⃣ - Book Car \n2️⃣ - View All User Booked Cars\n3️⃣ - View All Bookings" +
+                      "\n4️⃣ - View Available Cars\n5️⃣ - View Available Electric Cars\n6️⃣ - View all users\n7️⃣ - Exit");
+
+    }
+}
